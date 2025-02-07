d2042057e8df6d69b8a58feade40b2950dd60856
Petrik Diak <diak@petrik.hu>
2025-02-07 11:17:37 +0100

    Konyv nevű osztály létrehozva 2 konstruktorral 3 metódussal

--- OOPgyakorlas/Program.cs
+++ OOPgyakorlas/Program.cs
@@ -4,7 +4,16 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			Konyv konyv1 = new("A Kis Hereceg", "Antoine de Saint-Exupéry", 1943, 96, 2500);
+			Konyv kony2 = new("Ez még készül", "Én");
+
+            Console.WriteLine(konyv1.ToString() + "\n" + kony2.ToString());
+
+			konyv1.Kedvezmeny(20);
+			konyv1.Arnoveles(1000);
+
+			Console.WriteLine("\n" + konyv1.ToString() + "\n" + konyv1.HanyoldalMaradt(20));
+
 		}
 	}
 }
